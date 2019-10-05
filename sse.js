e1dd0816a69625cfcae9e9b886a36f4af6e515e1
Kasra Ghasemi <ghasemikasra39@yahoo.com>
2019-10-05 17:15:59 +0330

    mongo comment added

--- sse.js
+++ sse.js
@@ -191,7 +191,9 @@ app.get('/r1', (req, res, next) => {
     id++;
 });
 
-
+// echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+// sudo apt-get install -y mongodb-org
+// sudo service mongod start
 app.get('/r2', (req, res, next) => {
     let body1 = {
         for: 'PowerMeter',
